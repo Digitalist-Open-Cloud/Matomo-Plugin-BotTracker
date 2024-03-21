6633297aa0d6abd1d15692628d067e23fe288580
Mikke Schirén <mikke.schiren@digitalist.com>
2024-03-21 07:26:40 +0100

    add a basic phpunit test setup

---
+++ tests/Unit/ApiTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * BotTracker, a Matomo plugin by Digitalist Open Tech
+ * Based on the work of Thomas--F (https://github.com/Thomas--F)
+ * @link https://github.com/digitalist-se/MatomoPlugin-BotTracker
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\BotTracker\tests\Unit;
+
+use Piwik\Plugins\BotTracker\Api;
+
+class ApiTest extends \PHPUnit\Framework\TestCase
+{
+
+    private $api;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->api = new Api();
+    }
+
+
+    public function testGetApi(): void
+    {
+        $getAPI = $this->api;
+        $this->assertIsObject($getAPI);
+    }
+}
---
+++ tests/Unit/BotTrackerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * BotTracker, a Matomo plugin by Digitalist Open Tech
+ * Based on the work of Thomas--F (https://github.com/Thomas--F)
+ * @link https://github.com/digitalist-se/MatomoPlugin-BotTracker
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\BotTracker\tests\Unit;
+
+use Piwik\Plugins\BotTracker\BotTracker;
+
+class BotTrackerTest extends \PHPUnit\Framework\TestCase
+{
+
+    private $botTracker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->botTracker = new BotTracker();
+    }
+
+
+    public function testRegisterEvents(): void
+    {
+        $registerEvents = $this->botTracker->registerEvents();
+        $this->assertArrayHasKey('Tracker.isExcludedVisit', $registerEvents);
+        $this->assertArrayHasKey('Translate.getClientSideTranslationKeys', $registerEvents);
+    }
+}
